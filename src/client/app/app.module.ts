bc70494ed8227dc56be8d79d6ca2c57ef85fb026
Inthalak <inthalak.sirasa@gmail.com>
2017-07-30 10:59:53 +0200

    remove unused component

--- src/client/app/app.module.ts
+++ src/client/app/app.module.ts
@@ -2,22 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { APP_BASE_HREF } from '@angular/common';
 import { HttpModule } from '@angular/http';
-import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
-import { AboutModule } from './about/about.module';
-import { HomeModule } from './home/home.module';
+import { AppComponent } from './app.component';
+
 import { SharedModule } from './shared/shared.module';
 
 
 @NgModule({
-  imports: [BrowserModule, HttpModule, AppRoutingModule, AboutModule, HomeModule, SharedModule.forRoot()],
-  declarations: [AppComponent],
+  imports: [
+      BrowserModule,
+      HttpModule,
+      AppRoutingModule,
+      SharedModule.forRoot()
+  ],
+  declarations: [
+      AppComponent,
+  ],
   providers: [{
     provide: APP_BASE_HREF,
     useValue: '<%= APP_BASE %>'
   }],
-  bootstrap: [AppComponent]
+  bootstrap: [
+      AppComponent,
+  ]
 
 })
 export class AppModule { }
